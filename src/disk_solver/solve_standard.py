2f54a4ec19ea3d950896252d27a444fa3e0105a4
Cow <milkcowmilky@gmail.com>
2025-09-30 19:05:52 +0800

    feat:solvetools and solve standard.

--- src/disk_solver/solve_standard.py
+++ src/disk_solver/solve_standard.py
@@ -3,45 +3,53 @@ import math
 import numpy as np
 import scipy as sp
 
+from disk_solver import solve_tools
 from disk_solver.parameter_init import cgs_consts
 from parameters import model_params
 
-alpha_viscosity = model_params.alpha_viscosity[0]
-dimless_accrate = model_params.dimless_accrate[0]
-dimless_bhmass = model_params.dimless_bhmass[0]
-dimless_radius_out = model_params.dimless_radius_out[0]
-gas_index = model_params.gas_index[0]
 
-
-def get_bhmass(*, dimless_bhmass):
-    dimless_bhmass = np.asarray(dimless_bhmass)
-    bhmass = dimless_bhmass * cgs_consts.cgs_msun
-    return bhmass
-
-
-def get_accrate_edd(*, dimless_bhmass):
-    dimless_bhmass = np.asarray(dimless_bhmass)
-    accrate_edd = 1.44e18 * dimless_bhmass
-    return accrate_edd
-
-
-def get_accrate_init(*, dimless_accrate, dimless_bhmass):
-    dimless_accrate, dimless_bhmass = np.asarray(dimless_accrate), np.asarray(dimless_bhmass)
-    accrate_edd = get_accrate_edd(dimless_bhmass=dimless_bhmass)
-    accrate_init = accrate_edd * dimless_accrate
-    return accrate_init
-
-
-def get_radius_sch(*, dimless_bhmass, dimless_radius):
+def get_standard_angvel(*, dimless_bhmass, dimless_radius):
     dimless_bhmass, dimless_radius = np.asarray(dimless_bhmass), np.asarray(dimless_radius)
-    bhmass = get_bhmass(dimless_bhmass=dimless_bhmass)
-    radius_sch = 2 * cgs_consts.cgs_gra * bhmass / cgs_consts.cgs_c / cgs_consts.cgs_c
-    return radius_sch
+    bhmass = solve_tools.get_bhmass(dimless_bhmass=dimless_bhmass)
+    radius = solve_tools.get_radius_fromdimless(dimless_bhmass=dimless_bhmass, dimless_radius=dimless_radius)
+    standard_angvel = np.sqrt(cgs_consts.cgs_gra * bhmass / radius / radius / radius)
+    return standard_angvel
 
 
-def get_coeff_in(*, gas_index):
-    gas_index = np.asarray(gas_index)
-    numerator = (2**gas_index * sp.special.gamma(gas_index + 1)) ** 2
-    denominator = sp.special.gamma(2 * gas_index + 2)
-    coeff_in = numerator / denominator
-    return coeff_in
+def get_standard_pressure(*, standard_density, standard_temperature):
+    standard_density, standard_temperature = np.asarray(standard_density), np.asarray(standard_temperature)
+    part_1 = 2 * standard_density * cgs_consts.cgs_kb * standard_temperature / cgs_consts.cgs_mh
+    part_2 = cgs_consts.cgs_a * standard_temperature**4 / 3
+    standard_pressure = part_1 + part_2
+    return standard_pressure
+
+def get_standard_soundvel(*, standard_pressure, standard_density):
+    standard_pressure, standard_density = np.asarray(standard_pressure), np.asarray(standard_density)
+    standard_soundvel = np.sqrt(standard_pressure / standard_density)
+    return standard_soundvel
+
+def get_standard_averopacity(*, standard_density, standard_temperature):
+    standard_density, standard_temperature = np.asarray(standard_density), np.asarray(standard_temperature)
+    standard_averopacity = cgs_consts.cgs_kes + 6.4e22 * standard_density * standard_temperature ** (-3.5)
+    return standard_averopacity
+
+def get_standard_kineviscocity(*, standard_arealdensity, dimless_bhmass, dimless_accrate, dimless_radius):
+    standard_arealdensity = np.asanyarray(standard_arealdensity)
+    dimless_bhmass = np.asarray(dimless_bhmass)
+    dimless_accrate, dimless_radius = np.asarray(dimless_accrate), np.asarray(dimless_radius)
+    accrate = solve_tools.get_accrate_fromdimless(dimless_bhmass=dimless_bhmass, dimless_accrate=dimless_accrate)
+    part_1 = accrate / 3 / math.pi / standard_arealdensity
+    part_2 = 1 - np.sqrt(3 / dimless_radius)
+    standard_kineviscocity = part_1 * part_2
+    return standard_kineviscocity
+
+
+
+if __name__ == "__main__":
+    alpha_viscosity = model_params.alpha_viscosity[0]
+    dimless_accrate = model_params.dimless_accrate[0]
+    dimless_bhmass = model_params.dimless_bhmass[0]
+    gas_index = model_params.gas_index[0]
+    wind_index = model_params.wind_index[0]
+    dimless_radius_in = model_params.dimless_radius_in[0]
+    dimless_radius_out = model_params.dimless_radius_out[0]
---
+++ src/disk_solver/solve_tools.py
@@ -0,0 +1,52 @@
+import numpy as np
+import scipy as sp
+
+from disk_solver.parameter_init import cgs_consts
+
+
+def get_bhmass(*, dimless_bhmass):
+    dimless_bhmass = np.asarray(dimless_bhmass)
+    bhmass = dimless_bhmass * cgs_consts.cgs_msun
+    return bhmass
+
+
+def get_accrate_edd(*, dimless_bhmass):
+    dimless_bhmass = np.asarray(dimless_bhmass)
+    accrate_edd = 1.44e18 * dimless_bhmass
+    return accrate_edd
+
+
+def get_accrate_init(*, dimless_accrate, dimless_bhmass):
+    dimless_accrate, dimless_bhmass = np.asarray(dimless_accrate), np.asarray(dimless_bhmass)
+    accrate_edd = get_accrate_edd(dimless_bhmass=dimless_bhmass)
+    accrate_init = accrate_edd * dimless_accrate
+    return accrate_init
+
+
+def get_radius_sch(*, dimless_bhmass, dimless_radius):
+    dimless_bhmass, dimless_radius = np.asarray(dimless_bhmass), np.asarray(dimless_radius)
+    bhmass = get_bhmass(dimless_bhmass=dimless_bhmass)
+    radius_sch = 2 * cgs_consts.cgs_gra * bhmass / cgs_consts.cgs_c / cgs_consts.cgs_c
+    return radius_sch
+
+
+def get_coeff_in(*, gas_index):
+    gas_index = np.asarray(gas_index)
+    numerator = (2**gas_index * sp.special.gamma(gas_index + 1)) ** 2
+    denominator = sp.special.gamma(2 * gas_index + 2)
+    coeff_in = numerator / denominator
+    return coeff_in
+
+
+def get_radius_fromdimless(*, dimless_bhmass, dimless_radius):
+    dimless_bhmass, dimless_radius = np.asarray(dimless_bhmass), np.asarray(dimless_radius)
+    radius_sch = get_radius_sch(dimless_bhmass=dimless_bhmass, dimless_radius=dimless_radius)
+    radius = dimless_radius * radius_sch
+    return radius
+
+
+def get_accrate_fromdimless(*, dimless_bhmass, dimless_accrate):
+    dimless_bhmass, dimless_accrate = np.asarray(dimless_bhmass), np.asarray(dimless_accrate)
+    accrate_edd = get_accrate_edd(dimless_bhmass=dimless_bhmass)
+    accrate = accrate_edd * dimless_accrate
+    return accrate

f5ebe9e3784fbc734269ed227d067dd45a327ffe
Cow <milkcowmilky@gmail.com>
2025-10-13 17:34:35 +0800

    fix:ValueError: mutable default.

--- src/cold_disk/parameters/model_parameters.py
+++ src/cold_disk/parameters/model_parameters.py
@@ -21,20 +21,11 @@ array([0.1, 1.0, 10.0])
 array([0.1])
 
 """
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import numpy as np
 
 __all__ = ["model_params"]
-
-_alpha_viscosity = np.array([0.1])
-_dimless_accrate = np.array([0.1, 1, 10])
-_dimless_bhmass = np.array([1e6, 1e7])
-_gas_index = np.array([3.0])
-_wind_index = np.array([0.0])
-_dimless_radius_in = np.array([1.0])
-_dimless_radius_out = np.array([10000.0])
-
 
 @dataclass(frozen=True)
 class ModelParams:
@@ -59,13 +50,13 @@ class ModelParams:
 
     """
 
-    alpha_viscosity: np.ndarray = _alpha_viscosity
-    dimless_accrate: np.ndarray = _dimless_accrate
-    dimless_bhmass: np.ndarray = _dimless_bhmass
-    gas_index: np.ndarray = _gas_index
-    wind_index: np.ndarray = _wind_index
-    dimless_radius_in: np.ndarray = _dimless_radius_in
-    dimless_radius_out: np.ndarray = _dimless_radius_out
+    alpha_viscosity: np.ndarray = field(default_factory=lambda: np.array([0.1]))
+    dimless_accrate: np.ndarray = field(default_factory=lambda: np.array([0.1, 1, 10]))
+    dimless_bhmass: np.ndarray = field(default_factory=lambda: np.array([1e6, 1e7]))
+    gas_index: np.ndarray = field(default_factory=lambda: np.array([3.0]))
+    wind_index: np.ndarray = field(default_factory=lambda: np.array([0.0]))
+    dimless_radius_in: np.ndarray = field(default_factory=lambda: np.array([1.0]))
+    dimless_radius_out: np.ndarray = field(default_factory=lambda: np.array([10000.0]))
 
 
 model_params: ModelParams = ModelParams()
